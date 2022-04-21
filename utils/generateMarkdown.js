36d0284acbc4e7777e6e6c6b81dbe53285cdd0df
cdfoye <cdfoye@gmail.com>
2022-04-21 17:19:32 -0600

    created renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -27,11 +27,21 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  if (license) {
+    let section = `## License`;
+    let content = `License Link: ${renderLicenseLink(license)}`;
+
+    return section, content;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -74,9 +84,7 @@ function generateMarkdown(data) {
 
   For additional questions you can email me at ${data.email}
 
-  ## License
-  
-  ${data.license}`;
+  ${renderLicenseSection(data.license)}`;
 
 }
 
