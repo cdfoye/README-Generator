67f5d4e310ed4b24b5b85070146ad5a47910d4c5
cdfoye <cdfoye@gmail.com>
2022-04-21 02:46:30 -0600

    created array of questions for user input

--- index.js
+++ index.js
@@ -3,7 +3,55 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Write a short description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What kind of license should your project have?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What command should be run to install dependencies?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What command should be run to run tests?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What does the user need to know about contributing to the repo?',
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
